d981e789478042f4d486b024b0a4acc12718fb95
Arnold-A-Mubaiwa <arnoldmubaiwa99@gmail.com>
2021-12-10 06:43:20 +0200

    making the Vaccine table visible in the admin

--- wisdompets/adoptions/admin.py
+++ wisdompets/adoptions/admin.py
@@ -4,8 +4,10 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
-from .models import  Pet
+from .models import  Pet,Vaccine
 
 @admin.register(Pet)
 class PetAdmin (admin.ModelAdmin):
     list_display =['name','species','breed','age','sex']
+
+admin.site.register(Vaccine)
